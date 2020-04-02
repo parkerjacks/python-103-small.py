57cfd3eaa5cafb66d7c52d8e832a7e2409caf544
Park Jack <parker.jackson.hhs@gmail.com>
2020-04-02 11:44:49 -0400

    Added a file called square.py 5x5 square made of astericks and perfected the loop

---
+++ square.py
@@ -0,0 +1,12 @@
+#Print out a 5X5 square of * characters
+
+#Set It Up 
+square_is_made_of = '*'
+count = 0 
+
+#Work It Out 
+while count < 5: 
+    print(5 * square_is_made_of) 
+    count = count + 1 
+
+#Finish
