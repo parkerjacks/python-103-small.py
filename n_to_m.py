0613426c6bb52edbb14b0af68bb90d80231e5b74
Park Jack <parker.jackson.hhs@gmail.com>
2020-04-02 11:32:13 -0400

    created a file called n_to_m.py that prints out counting numbers from a user input start and end number

---
+++ n_to_m.py
@@ -0,0 +1,11 @@
+#Print counting numbers w/ user indicating which number to start and end
+num_start = int(input("What number would you like to start your count on?"))
+num_end = int(input("What number would you like to end your count on?"))
+
+
+#Work it out 
+while num_start <= num_end :
+    print (num_start)
+    num_start = num_start + 1
+
+#Finish 
